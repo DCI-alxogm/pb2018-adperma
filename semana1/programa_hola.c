91ac6c503d64ea82fb2fcc5828bc94a03954c5ec
Perez Maya Adrian <perezma2018@dci25-188.ugto.mx>
2018-08-08 09:25:25 -0500

    Creamos nuestro primer programa básico yey

---
+++ semana1/programa_hola.c
@@ -0,0 +1,10 @@
+/*
+ Creado el 06/08/2018 por Perez Maya Adrián
+*/
+#include<stdio.h>
+int main()
+{
+printf("HolaAdrián \n");
+printf("Este es el primer programa del curso\n");
+}
+/*Termine de escribir el programa*/
