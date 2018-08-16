ca073a0f17c07672344a348019b2eb8d6ecfa417
adperma <adrianperezmaya02@gmail.com>
2018-08-16 00:37:00 -0500

    /*Ejercicio 1 (Intento 1)*/

---
+++ semana2/edad2028.c
@@ -0,0 +1,15 @@
+/* Creado el 16 de Agosto por Adrián Pérez Maya*/
+ #include<stdio.h>
+
+int main()
+{
+	int edad; 
+
+	printf("Hola Adrián \n");
+	printf("ESte es el primer programa del curso");
+	printf("Introduce tu edad aquí: \n");
+	scanf("%i",&edad);
+	priintf("\nTu edad es: %i+10 \n");
+	return 0;
+	
+}
