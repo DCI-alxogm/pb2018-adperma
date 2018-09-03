6e415190e884e7120c68d968de1b612bc6c6c8f0
Perez Maya Adrian <perezma2018@dci25-188.ugto.mx>
2018-09-03 09:51:54 -0500

    //Uso de "for"

---
+++ semana5/ejemplo2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+	float Exp;
+	int n=10,j;
+
+	for(j=0;j<n;j++){
+		Exp=exp(j);
+		printf("%i \t %f\n",j,exp);
+	}
+
+	return 0;
+
+}
---
+++ semana5/ejercicio1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+	float x,intervalo,espaciado;
+
+	printf("Ingresa el valor de X: \n");
+	scanf("%f",x);
+
+	printf("Ingresa el intervalo: \n");
+	scanf("%f",intervalo);
+
+	printf("Ingresa el espaciado que deseas usar: ");
+	scanf("%f",espaciado);
+
+	for(x=
+
+
+	return 0;
+}
