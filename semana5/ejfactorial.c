8b9cd7db8fb16d1f1812342467932e84c45e8b69
Perez Maya Adrian <perezma2018@dci25-188.ugto.mx>
2018-09-10 09:16:49 -0500

    arrays

--- semana5/ejfactorial.c
+++ semana5/ejfactorial.c
@@ -3,22 +3,18 @@
 
 int main(){
 
+  int n,i,b;
 
+  printf("Ingresa un numero entero:\n ");
+  scanf("%i",&n);
 
+  if(n<0){
+	  printf("No hay factorial de numeros negativos \n");
+   }else{
+	   for(i=n;i<=1;i--){
+		   i*n=b
+			   printf(" %i",b);}
+   }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  return 0;
 }
