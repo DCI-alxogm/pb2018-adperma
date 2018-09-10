c71841f2c86ca29efb6797d1ba568a4066ed2cd8
Perez Maya Adrian <perezma2018@dci25-188.ugto.mx>
2018-09-10 09:49:08 -0500

    Ejemplo5

--- semana7/ejemplo5.c
+++ semana7/ejemplo5.c
@@ -2,4 +2,19 @@
 
 int main()
 {
-	int a
+	int sex;
+	int edad;
+	int semestre;
+	float calif;
+	float N;
+
+
+	printf("Ingresa el numero de alumnos a analizar: \n");
+	scanf("%f",&N);
+	for(i=0;i<N;i++){
+		printf("Al alumno es hombre(0) o mujer(1)\n");
+		scanf("%i",&sex);
+	}
+
+	return 0;
+}
