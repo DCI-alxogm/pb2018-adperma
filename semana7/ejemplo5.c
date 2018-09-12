d71d7dedf93b1740455225639dba41e578733941
adperma <adrianperezmaya02@gmail.com>
2018-09-12 13:34:12 -0500

    Actualización codigo ej5

--- semana7/ejemplo5.c
+++ semana7/ejemplo5.c
@@ -2,19 +2,78 @@
 
 int main()
 {
-	int sex;
-	int edad;
-	int semestre;
-	float calif;
-	float N;
+int sexo[10];
+float semestre[10];
+int edad[10];
+float calif[10];
+int i;
 
+	for(i=0;i<10;i++){
+	printf("%i Digite la edad del alumno en años: \n",i+1);
+	scanf("%i",&edad[i]);
+		}
 
-	printf("Ingresa el numero de alumnos a analizar: \n");
-	scanf("%f",&N);
-	for(i=0;i<N;i++){
-		printf("Al alumno es hombre(0) o mujer(1)\n");
-		scanf("%i",&sex);
+printf("\n\n");
+
+	for(i=0;i<10;i++){
+	printf("%i El alumno es hombre(0) o mujer(1) \n",i+1);
+	scanf("%i",&sexo[i]);
+	if(sexo[i]==0){ }
+	else{
+		if(sexo[i]==1){ }
+		else{
+			printf("El dato introucido es incorrecto \n");
+			i=i-1;
+				}
+		
+			}
 	}
 
-	return 0;
+printf("\n\n");
+
+	for(i=0;i<10;i++){
+	printf("%i Digite el semestre que cursa el alumno \n",i+1);
+	scanf("%f",&semestre[i]);
+	if(semestre[i]<1){
+		if(semestre[i]>9){
+	printf("Semestre No valido \n");}
+	else{}
+	}
+	}
+
+printf("\n\n");
+
+	for(i=0;i<10;i++){
+	printf("%i Ingrese el promedio actual obtenido en la carrera \n",i+1);
+	scanf("%f",&calif[i]);
+	if(0>calif[i]>10){
+	       	printf("Calificacion NO valida \n");
+	}
+	}
+
+printf("\n \n");
+
+	for(i=0;i<10;i++){
+	printf("%i \n",edad[i]);
+	}
+
+printf("\n \n");
+
+	for(i=0;i<10;i++){
+	printf("%i \n",sexo[i]);
+	}
+
+printf("\n \n");
+
+	for(i=0;i<10;i++){
+	printf("%f \n",semestre[i]);
+	}
+
+printf("\n \n");
+
+	for(i=0;i<10;i++){
+	printf("%.2f \n",calif[i]);
+	}
+
+return 0;
 }
