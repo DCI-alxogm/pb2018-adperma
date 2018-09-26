869946d2a018e01f03a76465de7331371b2930e7
Perez Maya Adrian <perezma2018@dci25-188.ugto.mx>
2018-09-26 07:45:30 -0500

    Update

--- semana7/EJfuncion.c
+++ semana7/EJfuncion.c
@@ -5,11 +5,31 @@ int main()
 {
 	FILE *datos;
 	FILE *resultados;
-	int p, a, b, N;
+	int p, a, b, N, k, i;
+	float x, delta;
 
 	datos=fopen("entrada.txt","r");
 
-	fscanf(datos, "%d",&p);
+	fscanf(datos, "%i",&p);
+	fscanf(datos, "%i",&a);
+	fscanf(datos, "%i",&b);
+	fscanf(datos, "%i",&N);
+
+	if(p<0){
+		printf("La potencia no es valida \n");
+		return 1;
+	}
+
+	delta=(b-a)/N;
+	       	/*Dependiendo del valor N de evaluaciones a realizar, el intervalo k cambia acorde a los limites a y b, delta representa el intervalo de avance para la variable k*/
+
+	for(i=0;i<N;i++) //Ciclo para el numero de evaluaciones de x
+	{
+		x=a+(delta*i);
+	}
+
+/*Ahora se debe de evaluar 
+
 
 	return 0;
 }
